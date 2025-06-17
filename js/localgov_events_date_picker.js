6e7c491aaaf13d78ffaa4990f352828fedbeffc8
Andy Broomfield <talk@andybroomfield.com>
2025-06-17 16:05:37 +0100

    Auto linting javascript fixes

--- js/localgov_events_date_picker.js
+++ js/localgov_events_date_picker.js
@@ -6,15 +6,12 @@
 (function ($) {
   Drupal.behaviors.localgovEventsDatePicker = {
     attach: function attach(context, settings) {
-
-      $('.js-date-picker').on('change', function() {
-
+      $('.js-date-picker').on('change', function () {
         let start_date = null;
         let end_date = null;
         const today = new Date();
 
-        switch(this.value) {
-
+        switch (this.value) {
           case 'today':
             start_date = today;
             end_date = today;
@@ -36,37 +33,49 @@
             break;
 
           case 'this_month':
-            start_date = new Date(today.getFullYear(), today.getMonth() , 1, 1);
-            end_date = new Date(today.getFullYear(), today.getMonth() + 1, 0, 1);
+            start_date = new Date(today.getFullYear(), today.getMonth(), 1, 1);
+            end_date = new Date(
+              today.getFullYear(),
+              today.getMonth() + 1,
+              0,
+              1,
+            );
             break;
 
           case 'next_month':
             if (today.getMonth() < 11) {
-              start_date = new Date(today.getFullYear(), today.getMonth() + 1, 1, 1);
-              end_date = new Date(today.getFullYear(), today.getMonth() + 2, 0, 1);
-            }
-            else {
+              start_date = new Date(
+                today.getFullYear(),
+                today.getMonth() + 1,
+                1,
+                1,
+              );
+              end_date = new Date(
+                today.getFullYear(),
+                today.getMonth() + 2,
+                0,
+                1,
+              );
+            } else {
               start_date = new Date(today.getFullYear() + 1, 0, 1, 1);
               end_date = new Date(today.getFullYear() + 1, 1, 0, 1);
             }
             break;
-
         }
 
         if (start_date) {
-          $('.js-date-picker-start').val(start_date.toISOString().substr(0, 10));
-        }
-        else {
+          $('.js-date-picker-start').val(
+            start_date.toISOString().substr(0, 10),
+          );
+        } else {
           $('.js-date-picker-start').val('');
         }
         if (end_date) {
           $('.js-date-picker-end').val(end_date.toISOString().substr(0, 10));
-        }
-        else {
+        } else {
           $('.js-date-picker-end').val('');
         }
       });
-
-    }
-  }
+    },
+  };
 })(jQuery);
