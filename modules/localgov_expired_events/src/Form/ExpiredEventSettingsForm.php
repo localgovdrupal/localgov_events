13f8efd9f90f54e567d02589fae4ebc66f505e27
Rosie Catchpole <43953393+CATCHPOLEBHC@users.noreply.github.com>
2025-07-24 12:26:40 +0100

    Codesniff cleanup

--- modules/localgov_expired_events/src/Form/ExpiredEventSettingsForm.php
+++ modules/localgov_expired_events/src/Form/ExpiredEventSettingsForm.php
@@ -1,14 +1,13 @@
 <?php
-/**
- * @file
- * Contains Drupal\localgov_expired_events\Form\ExpiredEventSettingsForm.
- */
 
 namespace Drupal\localgov_expired_events\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
+/**
+ * Configure expired event settings for this site.
+ */
 class ExpiredEventSettingsForm extends ConfigFormBase {
 
   /**
@@ -19,45 +18,46 @@ class ExpiredEventSettingsForm extends ConfigFormBase {
   }
 
   /**
-   * {@inheritdoc }
+   * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form = parent::buildForm($form, $form_state);
 
     $form['action'] = [
       '#type' => 'radios',
-      '#title' =>  $this->t('Action after Events are expired:'),
-      '#options' => array(
-        'none' => 'None',
+      '#title' => $this->t('Action after Events are expired:'),
+      '#options' => [
+        'none' => $this->t('None'),
         'unpublished' => $this->t('Unpublished'),
         'archived' => $this->t('Archived'),
         'delete' => $this->t('Delete'),
-      ),
-      '#config_target' => 'localgov_expired_events.settings:action'
+      ],
+      '#config_target' => 'localgov_expired_events.settings:action',
     ];
 
     $form['expire_days'] = [
       '#type' => 'textfield',
-      '#title' =>  $this->t('How many days will event be unpublished or deleted after expired ? '),
+      '#title' => $this->t('How many days will event be unpublished or deleted after expired ?'),
       '#description' => $this->t('Set 0 to delete or unpublished events right after events are expired.'),
-      '#config_target' => 'localgov_expired_events.settings:expire_days'
+      '#config_target' => 'localgov_expired_events.settings:expire_days',
     ];
 
     $form['items_per_cron'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Number of items processed per cron run:'),
-      '#config_target' => 'localgov_expired_events.settings:items_per_cron'
+      '#config_target' => 'localgov_expired_events.settings:items_per_cron',
     ];
 
     return parent::buildForm($form, $form_state);
   }
 
   /**
-   * {@inheritdoc }
+   * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
     return [
-      'localgov_expried_events.settings'
+      'localgov_expried_events.settings',
     ];
   }
+
 }
