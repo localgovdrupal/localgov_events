a6895b4b989170c42e2fe2381216c4d0cd155056
Rosie Catchpole <43953393+CATCHPOLEBHC@users.noreply.github.com>
2025-07-31 13:58:01 +0100

    First batch of tests

--- modules/localgov_expired_events/src/tests/Functional/DatesTest.php
+++ modules/localgov_expired_events/src/tests/Functional/DatesTest.php
@@ -88,6 +88,7 @@ class DatesTest extends BrowserTestBase {
     'date_recur',
     'content_moderation',
     'views',
+    'localgov_workflows',
     'workflows',
   ];
 
@@ -110,7 +111,7 @@ class DatesTest extends BrowserTestBase {
    */
   public function testExpiredEventArchived() {
 
-    $localgov_expired_event_date = [
+    $event_date = [
       'value' => '2022-07-22T16:00:00',
       'end_value' => '2022-07-22T18:00:00',
       'rrule' => '',
@@ -118,35 +119,127 @@ class DatesTest extends BrowserTestBase {
       'infinite' => 0,
       'rrule' => 'FREQ=DAILY;COUNT=10',
     ];
-  
-    $event1 = $this->drupalCreateNode([
+
+    $event = $this->drupalCreateNode([
       'type' => 'localgov_event',
       'title' => 'This event should be archived.',
       'body' => 'This event should be archived.',
       'status' => 1,
-      'localgov_event_date' => $localgov_expired_event_date,
+      'localgov_event_date' => $event_date,
+      'moderation_state' => 'published',
     ]);
-    $event1->save();
+    $event->save();
 
-    $this->assertTrue($event1->isPublished(), 'The event status is not correct');
+    $this->drupalGet('node/' . $event->id());
+    $this->assertSession()->statusCodeEquals(200, 'The event is not accessible.');
+    $this->assertTrue($event->isPublished(), 'The event status is should be published.');
+    $this->assertEquals("2022-07-22T18:00:00", $event->get('localgov_event_date')->end_value, 'Event end date is not correct.');
 
-    $this->drupalGet('node/' . $event1->id());
-    $this->assertSession()->statusCodeEquals(200, 'The event is not accessible.'); 
-
-    $this->assertEquals('2022-07-22T18:00:00', $event1->get('localgov_event_date')[0]->getValue()['end_value'], 'Event end date is not correct.');
-
-    // set up the configuration for expired events.
+    // Set up the configuration to archive events.
     $config = \Drupal::configFactory()->getEditable('localgov_expired_events.settings');
     $config->set('expire_days', 1)
       ->set('items_per_cron', 1)
       ->set('action', 'archived')
       ->save();
-    
+
     // Run cron to process expired events.
-    $this->cronRun(); 
+    $this->cronRun();
 
-    // set up content moderation state.
+    $refreshed_event = \Drupal::entityTypeManager()->getStorage('node')->load($event->id());
+    $this->assertFalse($refreshed_event->isPublished(), 'The event status is should be unpublished.');
+    // Check that the status is set to archived.
+    $this->assertEquals("0", $refreshed_event->get('status')->value, 'Event status should be archived');
 
-    // check that the event is archived.
   }
+
+  /**
+   * Tests that an expired event is archived.
+   */
+  public function testExpiredEventDeleted() {
+
+    $event_date = [
+      'value' => '2022-07-22T16:00:00',
+      'end_value' => '2022-07-22T18:00:00',
+      'rrule' => '',
+      'timezone' => 'Europe/London',
+      'infinite' => 0,
+      'rrule' => 'FREQ=DAILY;COUNT=10',
+    ];
+
+    $event = $this->drupalCreateNode([
+      'type' => 'localgov_event',
+      'title' => 'This event should be deleted.',
+      'body' => 'This event should be deleted.',
+      'status' => 1,
+      'localgov_event_date' => $event_date,
+      'moderation_state' => 'published',
+    ]);
+    $event->save();
+
+    $this->drupalGet('node/' . $event->id());
+    $this->assertSession()->statusCodeEquals(200, 'The event is not accessible.');
+    $this->assertTrue($event->isPublished(), 'The event status is should be published.');
+    $this->assertEquals("2022-07-22T18:00:00", $event->get('localgov_event_date')->end_value, 'Event end date is not correct.');
+
+    // Set up the configuration to delete events.
+    $config = \Drupal::configFactory()->getEditable('localgov_expired_events.settings');
+    $config->set('expire_days', 1)
+      ->set('items_per_cron', 1)
+      ->set('action', 'delete')
+      ->save();
+
+    // Run cron to process expired events.
+    $this->cronRun();
+
+    $refreshed_event = \Drupal::entityTypeManager()->getStorage('node')->load($event->id());
+    // Check that the status is set to archived.
+    $this->assertNull($refreshed_event, 'Event should be deleted.');
+
+  }
+
+  /**
+   * Tests that an expired event is unpublished.
+   */
+  public function testExpiredEventUnpublished() {
+
+    $event_date = [
+      'value' => '2022-07-22T16:00:00',
+      'end_value' => '2022-07-22T18:00:00',
+      'rrule' => '',
+      'timezone' => 'Europe/London',
+      'infinite' => 0,
+      'rrule' => 'FREQ=DAILY;COUNT=10',
+    ];
+
+    $event = $this->drupalCreateNode([
+      'type' => 'localgov_event',
+      'title' => 'This event should be deleted.',
+      'body' => 'This event should be deleted.',
+      'status' => 1,
+      'localgov_event_date' => $event_date,
+      'moderation_state' => 'published',
+    ]);
+    $event->save();
+
+    $this->drupalGet('node/' . $event->id());
+    $this->assertSession()->statusCodeEquals(200, 'The event is not accessible.');
+    $this->assertTrue($event->isPublished(), 'The event status is should be published.');
+    $this->assertEquals("2022-07-22T18:00:00", $event->get('localgov_event_date')->end_value, 'Event end date is not correct.');
+
+    // Set up the configuration to unpublish events.
+    $config = \Drupal::configFactory()->getEditable('localgov_expired_events.settings');
+    $config->set('expire_days', 1)
+      ->set('items_per_cron', 1)
+      ->set('action', 'unpublished')
+      ->save();
+
+    // Run cron to process expired events.
+    $this->cronRun();
+
+    $refreshed_event = \Drupal::entityTypeManager()->getStorage('node')->load($event->id());
+    // Check that the status is set to unpublished.
+    $this->assertEquals("1", $refreshed_event->get('status')->value, 'Event status should be unpublished');
+
+  }
+
 }
